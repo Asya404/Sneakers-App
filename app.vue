5634d5af39de696528630d4c10047e7668ca917d
Asya <unkiplum@gmail.com>
2024-02-01 11:36:38 +0100

    feat: add header

--- app.vue
+++ app.vue
@@ -1,5 +1,31 @@
 <template>
-  <div>
-    <NuxtWelcome />
+  <div class="container">
+    <Header />
   </div>
 </template>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+body {
+  background-color: #f7fee7;
+  font-family: "Inter", sans-serif;
+  font-size: 14px;
+}
+</style>
+
+<style scoped>
+.container {
+  margin: 0 auto;
+  background-color: #fff;
+  height: 100vh;
+  border-radius: 12px;
+  box-shadow: 0 20px 25px -5px rgb(0 0 0 / 0.1),
+    0 8px 10px -6px rgb(0 0 0 / 0.1);
+  margin-top: 55px;
+}
+</style>
---
+++ components/Header.vue
@@ -0,0 +1,72 @@
+<template>
+  <header>
+    <div class="logo">
+      <img src="/logo.png" alt="Logo" />
+      <div>
+        <h1>Headline</h1>
+        <p class="subtitle">Subtitle</p>
+      </div>
+    </div>
+    <ul>
+      <li>
+        <img src="/cart.svg" alt="Cart" />
+        <span><b>amount</b></span>
+      </li>
+      <li>
+        <img src="/heart.svg" alt="Heart" />
+        <span>favs</span>
+      </li>
+      <li>
+        <img src="/profile.svg" alt="Heart" />
+        <span>profile</span>
+      </li>
+    </ul>
+  </header>
+</template>
+
+<style scoped>
+header {
+  display: flex;
+  justify-content: space-between;
+  border-bottom: 1px solid #cbd5e1;
+  padding: 30px 40px;
+}
+
+.logo {
+  display: flex;
+  align-items: center;
+  gap: 15px;
+}
+
+.logo img {
+  width: 40px;
+}
+
+h1 {
+  font-size: 20px;
+  fweight: 700;
+  text-transform: uppercase;
+}
+
+.subtitle {
+  color: #9ca3af;
+}
+
+ul {
+  display: flex;
+  align-items: center;
+  gap: 30px;
+}
+
+ul li {
+  display: flex;
+  gap: 10px;
+  color: #6b7280;
+  transition: all 0.3s ease;
+  cursor: pointer;
+}
+
+ul li:hover {
+  color: #000;
+}
+</style>
